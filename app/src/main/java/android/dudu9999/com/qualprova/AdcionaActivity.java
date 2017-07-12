1445dde10bc57d8ec199d59e7eb66844f4ea3389
dudu9999 <ecaetanocorrea@gmail.com>
2017-07-12 14:28:22 -0300

    create integrate of firebase

---
+++ app/src/main/java/android/dudu9999/com/qualprova/AdcionaActivity.java
@@ -0,0 +1,13 @@
+package android.dudu9999.com.qualprova;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AdcionaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adciona);
+    }
+}
